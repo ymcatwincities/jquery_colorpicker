475e9215b6dc665f0d7ac771c80267543020456b
JayMatwichuk <JayMatwichuk@324696.no-reply.drupal.org>
2017-02-02 10:46:28 +0900

    Issue #2848855 by Jaypan: No widget available for: jquery_colorpicker

--- src/Plugin/Field/FieldType/JQueryColorpickerItem.php
+++ src/Plugin/Field/FieldType/JQueryColorpickerItem.php
@@ -7,6 +7,7 @@ use Drupal\Core\Field\FieldItemInterface;
 use Drupal\Core\Field\FieldStorageDefinitionInterface;
 use Drupal\Core\TypedData\DataDefinition;
 
+
 /**
  * Provides the jquery_colorpicker field type
  *
